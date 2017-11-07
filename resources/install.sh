c56642d01b2044179d240303bf8cd4ec9028b065
Suman Roy <suman.roy@nytimes.com>
2017-11-06 22:52:34 -0500

    adds convenient shell scripts

--- resources/install.sh
+++ resources/install.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
+brew install go;
+
+go get github.com/mantithetical/tpt;
 cd $GOPATH/src/github.com/mantithetical/tpt;
 got get ./...;
 go install;
 
-if brew ls --versions myformula > /dev/null; then
-  # The package is installed
+cp resources/.tpt.yaml $HOME/.;
+
+if brew ls --versions bash-completion > /dev/null; then
+    brew install bash-completion;
 fi
+
+cp resources/tpt_bash_completion.sh $(brew --prefix)/etc/bash_completion.d/tpt_bash_completion.sh;
+
+touch $HOME/.bashrc;
+cat <<EOT >> $HOME/.bashrc
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+. $(brew --prefix)/etc/bash_completion
+fi
+EOT
--- resources/uninstall.sh
+++ resources/uninstall.sh
@@ -2,13 +2,29 @@
 
 # Supports Mac OSs only
 
+declare -a deps=("c9s" "fsnotify" "jdkato" "magiconair" "mitchellh" "pelletier"
+"spf13")
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    pkg="$GOPATH/pkg/darwin_amd64/github.com/mantithetical/tpt"
-    echo "Deleting $pkg..."
-    rm -fr $pkg;
+    for i in "${deps[@]}"
+    do
+        pkg="$GOPATH/pkg/darwin_amd64/github.com/$i"
+        src="$GOPATH/src/github.com/$i"
+        echo "Deleting $pkg..."
+        rm -fr $pkg;
+        echo "Deleting $src..."
+        rm -fr $src;
+    done
+
     bin="$GOPATH/bin/tpt...";
     echo "Deleting $bin";
     rm -f $bin;
+
+    rm -f $HOME/.tpt.yaml
+
+    if brew ls --versions bash-completion > /dev/null; then
+      rm -f $(brew --prefix)/etc/bash_completion.d/tpt_bash_completion.sh
+    fi
 else
     echo "$OSTYPE is not supported";
 fi
